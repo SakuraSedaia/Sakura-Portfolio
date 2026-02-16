5ba4de09dccfbe1a71e41e97f794775c6a7a4537
SakuraSedaia <sakusedaia@outlook.com>
2026-02-15 17:08:55 -0800

    Added styling for Render Card component, improved JSON description handling, and updated Asset Description Content.

--- src/components/render-card.jsx
+++ src/components/render-card.jsx
@@ -2,20 +2,27 @@ import { Show } from "solid-js";
 
 export default function RenderCard(props) {
 	const json = JSON.parse(props.data)
-	const folder = json.name.toLowerCase().replace(" ", "-");
-	
+	const folder = json.name.toLowerCase().replaceAll(" ", "-");
+	let showDesc = false
+	if (json.description !== "") {
+		showDesc = true
+	}
   return (
-    <div class={"render-card grid-item"}>
+    <div 
+			class={"render-card grid-item"} 
+			style={props.maxHeight ? { "--render-card-max-height": props.maxHeight } : {}}
+		>
       <h2>{json.name}</h2>
 			<a href="#" onClick={(e) => { e.preventDefault(); props.onImageClick(json, props.cat, folder); }}>
 				<img src={`/images/renders${props.cat}${folder}/${json.sizes[0]}`} alt={json.name} />
 				
 			</a>
 	    <div class={"detail"}>
-		    <span>Release Date: {json.month} {json.year.toString()}</span>
+		    <span>Created in {json.month} {json.year.toString()}</span>
 	    </div>
-	    <p>{json.description}</p>
-	    
+	    <Show when={showDesc}>
+	      <p>{json.description}</p>
+	    </Show>
 	    <Show when={props.debug}>
 		    <pre>/images/renders{props.cat}{folder}/{json.sizes[0]}</pre>
 		    <pre>{JSON.stringify(json, null, 2)}</pre>
