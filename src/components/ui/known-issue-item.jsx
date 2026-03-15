f6ea9e73d51a10e098250a539319715006066b75
Sakura Sedaia <sakusedaia@outlook.com>
2026-03-15 16:00:37 -0500

    Updated Known Issues section: removed outdated issues, added "No Known Issues" placeholder, and improved link handling in `KnownIssueItem` component.

--- src/components/ui/known-issue-item.jsx
+++ src/components/ui/known-issue-item.jsx
@@ -2,14 +2,23 @@ import Tooltip from "../navigation/tooltip.jsx";
 
 export default function KnownIssueItem(props) {
 	const issueNumber = props.issueLink.substring(props.issueLink.lastIndexOf("/") + 1);
+	let isLinkDisabled = false;
+	if (props.issueLink == "disabled") {
+		isLinkDisabled = true;
+	}
 	return (
 		<li class={"known-issue-item"}>
 			<h3>
-				<Tooltip text={`Github Issue #${issueNumber}`}>
-					<a href={props.issueLink} class={"link"} target={"_blank"}>
-						{props.title}
-					</a>
-				</Tooltip>
+				{isLinkDisabled ? (
+					<span>{props.title}</span>
+					) : (
+					<Tooltip text={`Github Issue #${issueNumber}`}>
+						
+							<a href={props.issueLink} class={"link"} target={"_blank"}>
+								{props.title}
+							</a>
+					</Tooltip>
+				)}
 			</h3>
 			<h4>Confirmed on {props.confirmed}</h4>
 			<div>
--- src/sections/blender-development/known-issues.jsx
+++ src/sections/blender-development/known-issues.jsx
@@ -12,41 +12,18 @@ export default function BlenderDevKnownIssues() {
 								<h2>Known Issues</h2>
 							</div>
 							<p>
-								Currently there are two big issues that are currently impacting user experience, though there are
+								Currently there are no known issues impacting user experience, though there are
 								guaranteed to be more as the plugin is tested and stressed. Issues listed here are confirmed,
 								verified, and are either planned or are currently being worked on.
 							</p>
 							<div class={"known-issues-list"}>
 								<ul>
-									<KnownIssueItem
-										title={"Softlock in Project Wizard API"}
-										confirmed={"03-08-2026"}
-										issueLink={"https://github.com/SakuraSedaia/blender_pycharm/issues/1"}
-									>
+									<KnownIssueItem title={"No Known issues as this time."} confirmed={"MM-DD-YYYY"} issueLink={"disabled"}>
+										{/*
+										The issueLink attribute can either be a proper link or "disabled". If set to disabled, the tooltip and link are disabled, and no longer can be clicked.
+										*/}
 										<p>
-											It is possible to softlock the New Project Wizard by performing these steps.
-										</p>
-										<ol>
-											<li>Start new Project Wizard (<code>File > New Project</code>)</li>
-											<li>Select the Blender Extension template</li>
-											<li>Toggle one of the 5 permission options (Files, Network, etc)</li>
-											<li>While leaving the Permission Reason blank, click Create</li>
-										</ol>
-										<p>
-											This issue is a big one which impacts user experience, as it prevents the smooth creation of new
-											projects. The fix for this issue is a complex one, as the logic for the New Project Wizard needs
-											to be rewritten to facilitate more efficient and robust field validation.
-										</p>
-									</KnownIssueItem>
-									<KnownIssueItem
-										title={"Cannot Download Blender Versions through MBVS"}
-										confirmed={"03-08-2026"}
-										issueLink={"https://github.com/SakuraSedaia/blender_pycharm/issues/2"}
-									>
-										<p>
-											Users are unable to download Blender versions through the MBVS, which is a critical issue as it
-											prevents users from accessing sandboxed installations separate from their main install. Currently,
-											the issue has no fix as I have not had time to diagnose and resolve the issue.
+											No known issues as of now, this page will be updated as more issues are discovered and logged.
 										</p>
 									</KnownIssueItem>
 								</ul>
