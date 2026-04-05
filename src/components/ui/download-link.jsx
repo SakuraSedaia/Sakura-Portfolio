cedb3f3ebe9f8774bca2c0f0abe5b04c9359fc15
SakuraSedaia <sakusedaia@outlook.com>
2026-04-05 04:11:42 -0500

    Extract filename for `download` attribute in DownloadLink component
    Signed-off-by: SakuraSedaia <sakusedaia@outlook.com>

--- src/components/ui/download-link.jsx
+++ src/components/ui/download-link.jsx
@@ -1,10 +1,17 @@
 export default function DownloadLink(props) {
   const downloadPath = `/lib/${props.path}`;
   
+  // Extract filename from path to use as the download attribute
+  const fileName = () => {
+    if (!props.path) return undefined;
+    const parts = props.path.split('/');
+    return parts[parts.length - 1];
+  };
+  
   return (
     <a 
       href={props.disabled ? "#" : downloadPath} 
-      download={props.disabled ? undefined : true}
+      download={props.disabled ? undefined : fileName()}
       class={`${props.class || ""} ${props.disabled ? "disabled" : ""}`}
       onClick={(e) => { if (props.disabled) e.preventDefault(); }}
     >
