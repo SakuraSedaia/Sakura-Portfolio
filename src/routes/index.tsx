9ebc8b22df4927bb8f74c14074925f104b9eca3b
SakruaSedaia <sakusedaia@outlook.com>
2026-06-27 23:35:07 -0500

    [Feature -> routes]: Replace placeholder content with structured sections on the home page
    Signed-off-by: Sakura Sedaia <sakusedaia@outlook.com>

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,19 +1,19 @@
-import { Title } from "@solidjs/meta";
-import Counter from "~/components/Counter";
+import Metadata from "~/components/page-metadata";
+import Hero from "~/components/sections/index/hero";
+import Projects from "~/components/sections/index/projects";
+import Commissions from "~/components/sections/index/comissions";
+import Contact from "~/components/sections/index/contact";
+
 
 export default function Home() {
+  
   return (
-    <main>
-      <Title>Hello World</Title>
-      <h1>Hello world!</h1>
-      <Counter />
-      <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
-      </p>
+      
+      {/* Page Sections */}
+      <Hero />
+      <Projects />
+      <Commissions />
+      <Contact />
     </main>
   );
 }
