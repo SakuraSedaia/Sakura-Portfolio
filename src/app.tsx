9982a08f7f9a5200fdabffdf54c4153859bf331c
SakruaSedaia <sakusedaia@outlook.com>
2026-06-27 23:35:35 -0500

    [Feature -> components]: Add navigation bar and footer components to enhance layout and routing
    Signed-off-by: Sakura Sedaia <sakusedaia@outlook.com>

--- src/app.tsx
+++ src/app.tsx
@@ -1,18 +1,21 @@
-import { MetaProvider, Title } from "@solidjs/meta";
+import { MetaProvider } from "@solidjs/meta";
 import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
-import { Suspense } from "solid-js";
-import "./app.css";
+import { ErrorBoundary, Suspense } from "solid-js";
+import "./app.scss";
+import NavBar from "~/components/sections/common/nav-bar";
+import Footer from "~/components/sections/common/footer";
 
 export default function App() {
   return (
     <Router
-      root={props => (
+      root={(props) => (
         <MetaProvider>
-          <Title>SolidStart - Basic</Title>
-          <a href="/">Index</a>
-          <a href="/about">About</a>
-          <Suspense>{props.children}</Suspense>
+          <NavBar />
+          <ErrorBoundary fallback={<div>Something went wrong</div>}>
+            <Suspense>{props.children}</Suspense>
+          </ErrorBoundary>
+          <Footer />
         </MetaProvider>
       )}
     >
