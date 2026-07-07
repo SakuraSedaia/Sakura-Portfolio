91b59733d964d4da731247ef561ff4663b5386d9
Sakura Sedaia <sakusedaia@outlook.com>
2026-07-06 21:57:28 -0500

    [pnpm]: Updated SolidStart to V2.0.0-alpha.3, Vite to 8.0.5, and vite-plugin-nitro-2 to 0.3.0
    Signed-off-by: Sakura Sedaia <sakusedaia@outlook.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from "vite";
 import { nitroV2Plugin } from "@solidjs/vite-plugin-nitro-2";
-
 import { solidStart } from "@solidjs/start/config";
 
 export default defineConfig({
-  plugins: [solidStart(), nitroV2Plugin()],
+  plugins: [
+    solidStart(),
+    nitroV2Plugin({
+      preset: "vercel",
+    }),
+  ],
 });
