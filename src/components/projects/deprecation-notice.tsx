123d64475bad8f65f8d37500ecb146f1207c606b
Sakura Sedaia <sakusedaia@outlook.com>
2026-07-11 15:52:27 -0500

    [Refactor]: Wrapped multiple components with NoHydration for improved SSR performance and hydration control.
    Signed-off-by: Sakura Sedaia <sakusedaia@outlook.com>

--- src/components/projects/deprecation-notice.tsx
+++ src/components/projects/deprecation-notice.tsx
@@ -1,8 +1,12 @@
+import { NoHydration } from "solid-js/web";
+
 export default function DeprecationNotice(props: { message: string }) {
   return (
-    <div class="deprecation-notice">
-      <h1>Deprecation Notice:</h1>
-      <p>{props.message}</p>
-    </div>
+    <NoHydration>
+      <div class="deprecation-notice">
+        <h1>Deprecation Notice:</h1>
+        <p>{props.message}</p>
+      </div>
+    </NoHydration>
   );
 }
--- src/components/sections/projects/sakura-character-rig/addon.tsx
+++ src/components/sections/projects/sakura-character-rig/addon.tsx
@@ -1,20 +1,23 @@
 import { A } from "@solidjs/router";
+import { NoHydration } from "solid-js/web";
 
 export default function Addon() {
   return (
-    <section id={"sacr-addon"}>
-      <div class={"standard-container"}>
-        <div class={"issues-box"}>
-          <h1>Get the Utilities</h1>
-          <p>
-            To get the most out of the Sakura Character Rig, we recommend using
-            the Sakura Rig Utilities extension for Blender.
-          </p>
-          <A href={"/projects/sakura-rig-utilities"} class={"btn-primary"}>
-            View Sakura Rig Utilities
-          </A>
+    <NoHydration>
+      <section id={"sacr-addon"}>
+        <div class={"standard-container"}>
+          <div class={"issues-box"}>
+            <h1>Get the Utilities</h1>
+            <p>
+              To get the most out of the Sakura Character Rig, we recommend
+              using the Sakura Rig Utilities extension for Blender.
+            </p>
+            <A href={"/projects/sakura-rig-utilities"} class={"btn-primary"}>
+              View Sakura Rig Utilities
+            </A>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </NoHydration>
   );
 }
